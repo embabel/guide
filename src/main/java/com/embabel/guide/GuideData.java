21085e155c5c2d4f692e7ef0dd94a17b170301b3
Rod Johnson <johnsonroda@gmail.com>
2025-11-22 12:58:13 +1100

    Bring in launchpad features

--- src/main/java/com/embabel/guide/GuideData.java
+++ src/main/java/com/embabel/guide/GuideData.java
@@ -1,17 +1,14 @@
 package com.embabel.guide;
 
 import com.embabel.agent.api.common.LlmReference;
+import com.embabel.agent.api.common.reference.LlmReferenceProviders;
 import com.embabel.agent.api.identity.User;
 import com.embabel.agent.rag.ingestion.DirectoryParsingConfig;
 import com.embabel.agent.rag.ingestion.DirectoryParsingResult;
 import com.embabel.agent.rag.ingestion.TikaHierarchicalContentReader;
 import com.embabel.agent.rag.service.RagService;
 import com.embabel.agent.rag.store.ChunkingContentElementRepository;
-import com.embabel.agent.rag.tools.RagOptions;
 import com.embabel.agent.tools.file.FileTools;
-import com.embabel.coding.tools.api.ApiReference;
-import com.embabel.coding.tools.git.RepositoryReferenceProvider;
-import com.embabel.coding.tools.jvm.ClassGraphApiReferenceExtractor;
 import com.google.common.collect.Iterables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Exposes the guide configuration and the loaded references
@@ -35,44 +30,26 @@ public class GuideData {
 
     private final Logger logger = LoggerFactory.getLogger(GuideData.class);
     private final GuideConfig guideConfig;
-    private final List<LlmReference> references = new LinkedList<>();
+    private final List<LlmReference> references;
     private final ChunkingContentElementRepository store;
     private final PlatformTransactionManager platformTransactionManager;
-    private final RagService ragService;
+    private final RagService embabelContentRagService;
 
     public GuideData(
             ChunkingContentElementRepository store,
             GuideConfig guideConfig,
             PlatformTransactionManager platformTransactionManager,
-            RagService ragService) {
+            RagService embabelContentRagService) {
         this.store = store;
         this.guideConfig = guideConfig;
         this.platformTransactionManager = platformTransactionManager;
-        var embabelAgentApiReference = new ApiReference(
-                "Embabel Agent API: Core",
-                new ClassGraphApiReferenceExtractor().fromProjectClasspath(
-                        "embabel-agent",
-                        Set.of("com.embabel.agent", "com.embabel.common"),
-                        Set.of()),
-                100);
-        references.add(embabelAgentApiReference);
-        this.ragService = ragService;
-
-        // TODO this could be data driven
-        addGithubReference("https://github.com/embabel/embabel-agent-examples.git", "Embabel examples repo");
-        addGithubReference("https://github.com/embabel/embabel-agent.git", "Embabel agent implementation repo: Look to check code under embabel-agent-api");
+        this.references = LlmReferenceProviders.fromYmlFile(guideConfig.referencesFile());
+        this.embabelContentRagService = embabelContentRagService;
     }
 
-    private void addGithubReference(@NonNull String repoUrl, @NonNull String description) {
-        try {
-            var examplesReference = RepositoryReferenceProvider.create()
-                    .cloneRepository(repoUrl, description);
-            references.add(examplesReference);
-            logger.info("Loaded Github repo {} for tool access", repoUrl);
-        } catch (Throwable t) {
-            // Allows working offline
-            logger.warn("Failed to load Github repo {} for tool access", repoUrl);
-        }
+    @NonNull
+    public RagService embabelContentRagService() {
+        return embabelContentRagService;
     }
 
     @NonNull
@@ -113,12 +90,6 @@ public class GuideData {
             }
             return directoryParsingResult;
         });
-    }
-
-    public RagOptions ragOptions() {
-        return new RagOptions(ragService)
-                .withSimilarityThreshold(guideConfig.similarityThreshold())
-                .withTopK(guideConfig.topK());
     }
 
 }
