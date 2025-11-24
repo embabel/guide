5a42db995dbf6979b30e0156390a51a8c211f7a2
jasper blues <jasper@liberation-data.com>
2025-11-24 18:08:29 +1000

    Fix test?

--- src/test/kotlin/com/embabel/hub/HubApiControllerTest.kt
+++ src/test/kotlin/com/embabel/hub/HubApiControllerTest.kt
@@ -6,7 +6,9 @@ import com.embabel.guide.domain.drivine.DrivineGuideUserRepository
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.springframework.ai.mcp.client.common.autoconfigure.McpClientAutoConfiguration
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
@@ -21,7 +23,7 @@ import org.springframework.transaction.annotation.Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 @ContextConfiguration(initializers = [Neo4jPropertiesInitializer::class])
-//@ImportAutoConfiguration(exclude = [McpClientAutoConfiguration::class])
+@ImportAutoConfiguration(exclude = [McpClientAutoConfiguration::class])
 @Transactional
 class HubApiControllerTest {
 
